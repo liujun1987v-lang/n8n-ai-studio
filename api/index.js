a3153ef204547703e4139dc5b589510f224d67c7
liujun1987v-lang <276919762+liujun1987v-lang@users.noreply.github.com>
2026-05-04 07:33:29 -0400

    Simplify Vercel configuration for single serverless function

--- api/index.js
+++ api/index.js
@@ -1,2 +1,19 @@
-import app from "../server/_core/index.ts";
+import app from '../server/_core/index.ts';
+import path from 'path';
+import express from 'express';
+
+const distPath = path.join(process.cwd(), 'dist', 'public');
+app.use(express.static(distPath));
+
+app.get('*', (req, res, next) => {
+  if (req.path.startsWith('/api')) {
+    return next();
+  }
+  res.sendFile(path.join(distPath, 'index.html'), (err) => {
+    if (err) {
+      next();
+    }
+  });
+});
+
 export default app;
