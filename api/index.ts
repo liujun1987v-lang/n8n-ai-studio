1121c6358d3cd8ad07c83e8dbe3b6cd53d3ddedb
liujun1987v-lang <276919762+liujun1987v-lang@users.noreply.github.com>
2026-05-04 07:36:52 -0400

    Final attempt with standard Vercel configuration

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,2 @@
 import app from '../server/_core/index';
-import path from 'path';
-import express from 'express';
-
-const distPath = path.join(process.cwd(), 'dist', 'public');
-app.use(express.static(distPath));
-
-app.get('*', (req, res, next) => {
-  if (req.path.startsWith('/api')) {
-    return next();
-  }
-  res.sendFile(path.join(distPath, 'index.html'), (err) => {
-    if (err) {
-      next();
-    }
-  });
-});
-
 export default app;
